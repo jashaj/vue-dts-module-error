b24320a785855048bbf4ff879daa4977f96c89c5
Jasha Joachimsthal <jasha.joachimsthal@sue.nl>
2024-07-10 11:03:11 +0200

    Add config for dts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,37 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'MyLib',
+      formats: ['es'],
+      fileName: 'index'
+    },
+    rollupOptions:{
+      external:['vue','pinia'],
+      output: {
+        exports: 'named',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          pinia: 'pinia',
+        },
+      },
+    }
+  },
   plugins: [
     vue(),
+    dts({
+      entryRoot:'./src/lib',
+      copyDtsFiles:true
+    })
   ],
   resolve: {
     alias: {
